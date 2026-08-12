6470d4d9697a583a25249c655d9ec9adc576aaa3
Luqmanbrats <fannyskylark1659@gmail.com>
2026-08-12 11:41:32 +0800

    lab06

--- labs/lab06/exercise1/exercise1.py
+++ labs/lab06/exercise1/exercise1.py
@@ -1,4 +1,31 @@
-# Escape Characters Exercise
-# Print the receipt shown in the lab, using \n for new lines and \t for columns.
-# Calculate every total, subtotal, and tax in your code. Do not type the money
-# amounts in directly. Show every amount with exactly two decimal places.
+coffee_name="coffee"
+coffee_price=3.50
+coffee_qty=2
+coffee_total=coffee_price*coffee_qty
+
+muffin_name="Muffin"
+muffin_price=2.10
+muffin_qty=3
+muffin_total=muffin_price*muffin_qty
+
+water_name="water"
+water_price=1.05
+water_qty=4
+water_total=water_price*water_qty
+
+subtotal=coffee_total+muffin_total+water_total
+tax=subtotal*0.06
+total=subtotal+tax
+
+print(
+    f"========== RECEIPT ==========",
+    f"\nItem\tPrice\tqty\tTotal",
+    f"\n{coffee_name}\t{coffee_price}\t{coffee_qty}\t{coffee_total}",
+    f"\n{muffin_name}\t{muffin_price:.2f}\t{muffin_qty}\t{muffin_total:.2f}",
+    f"\n{water_name}\t{water_price}\t{water_qty}\t{water_total}",
+    f"\n------------------------------",
+    f"\nsubtotal\t{subtotal}",
+    f"\ntax (6%)\t{tax}",
+    f"\nTotal\t{total}",
+    f"\n==============================",
+)
