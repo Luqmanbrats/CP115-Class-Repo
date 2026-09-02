15d1279baaeb85d9bfa3f6b4918243ac0ccec37c
Luqmanbrats <fannyskylark1659@gmail.com>
2026-09-02 11:38:40 +0800

    labs

--- labs/lab07/exercise.py
+++ labs/lab07/exercise.py
@@ -1,2 +1,7 @@
-# Lab 07 scratch file
-# Use this file to try out the code from the lab as you follow along.
+value = 1
+
+if(value == 1):
+     print("Value is 1")
+       print("This is an additional print statement.")
+else:
+  print("Value is not 1")
