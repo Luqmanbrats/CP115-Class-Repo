bfd9fbde6cce120a8f153732da5cea44e3e95f2d
Luqmanbrats <fannyskylark1659@gmail.com>
2026-09-23 11:34:46 +0800

    labs

--- labs/lab08/exercise1/exercise1.py
+++ labs/lab08/exercise1/exercise1.py
@@ -1,8 +1,13 @@
+student_name = input("Enter Your Name")
+gpa = float(input("Enter Your GPA"))
+credit_hours = int(input("Enter Your Credit Hours"))
 
-student_name = input()
-gpa = float(input())
-credit_hours = int(input())
-
-
-
+if gpa >=3.8 and credit_hours>=12:
+    classification="Dean List"
+elif gpa>=3.5 and credit_hours>=12:
+    classification="Honor Roll"
+elif gpa>=2.0:
+    classification="Good Standing"
+else:
+    classification="Academic Probation"    
 print(classification)
