1a4b6f694593ecc8c025b0cbce7129d6841468b2
Luqmanbrats <fannyskylark1659@gmail.com>
2026-09-23 12:02:38 +0800

    labs

--- labs/lab08/exercise1/exercise1.py
+++ labs/lab08/exercise1/exercise1.py
@@ -1,9 +1,9 @@
-student_name = input("Enter Your Name")
-gpa = float(input("Enter Your GPA"))
-credit_hours = int(input("Enter Your Credit Hours"))
+student_name = input()
+gpa = float(input())
+credit_hours = int(input())
 
 if gpa >=3.8 and credit_hours>=12:
-    classification="Dean List"
+    classification="Dean's List"
 elif gpa>=3.5 and credit_hours>=12:
     classification="Honor Roll"
 elif gpa>=2.0:
--- labs/lab08/exercise2/exercise2.py
+++ labs/lab08/exercise2/exercise2.py
@@ -2,7 +2,25 @@ employee_name = input()
 base_salary = float(input())
 overtime_hours = int(input())
 tax_status = input()
-
+gross = base_salary + (overtime_hours*35)
+epf = 0.11
+socso = 0.005
+if tax_status=="Single":
+    if gross>=5000:
+        tax_rate=0.22
+    else:
+        tax_rate=0.18
+elif tax_status=="Married":
+    if gross>=6000:
+        tax_rate=0.20
+    else:
+        tax_rate=0.15
+else:
+    if gross>=5500:
+        tax_rate=0.25
+    else:
+        tax_rate=0.19
+net_salary=gross -(gross*tax_rate)-(gross*epf)-(gross*socso)
 
 
 print(employee_name)
